b5678329b030fbf411178ad7bfb9981a969b7365
emersonflemi <87089419+emersonflemi@users.noreply.github.com>
2021-07-20 15:45:03 -0400

    cleaned up files (deleted redundancies)

--- Code for HW.R
+++ Code for HW.R
@@ -3,6 +3,4 @@ library(distill)
 library(postcards)
 blogbuilder::exclude_docs()
 iamstudent()
-distill::create_post(title = 'haha', draft = TRUE)
-
-
+create_post(title = 'eflemingtest_hw2', draft = TRUE)
